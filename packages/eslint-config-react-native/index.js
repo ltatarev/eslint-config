6845c0ddfd8b9dc9e335a76514bfd338d878fcdc
Lucija Tatarević <lucija.tatarevic@gmail.com>
2023-02-01 13:04:34 +0100

    Add packages init

---
+++ packages/eslint-config-react-native/index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "@ltatarev/react",
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: [
+    "react",
+    "react-native",
+    "import",
+    "simple-import-sort",
+    "unused-imports"
+  ],
+  env: {
+    "react-native/react-native": true
+  },
+  rules: {
+    "react-native/no-unused-styles": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-raw-text": 1,
+    "react-native/no-color-literals": 2,
+    "react-native/no-single-element-style-arrays": 2,
+  },
+};
